7a1998ad52e475cea9595f767cf7d0b40ae0362d
htkitty <eulfs@proton.me>
2026-04-28 21:38:50 +0200

    kernel 1.0.4

--- kernel.c
+++ kernel.c
@@ -1,17 +1,18 @@
-#define lpb vgbp[lp++]=
-#define kpf(st) do{ \
-       	char *let = (char*)(st); \ 
-	while(*let){ \
-		lpb *let++; \
-		lpb 0x07; } } while(0)
-#define kpn lp += 160
+#include "estd.h"
 extern int main(){
+	//essential initialization
+	unsigned char *let;
+	INIT_LET(let);
 	volatile char *vgbp = (char*)0xB8000;
 	int lp = 0;
+	int vgbl = 1;
 
+	//example code
+	kcl();
 	kpf("Welcome to MathDOS");
 	kpn;
-	kpf("hiiiiii");
+	kpf("==================================");
+	kpn; kpn;
+	kpf("Open-Source Disk Operating System");
 	while(1);
-
 }
